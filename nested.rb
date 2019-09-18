9892ddb335ce2753092316c533a04edd00f5baac
Gene H Li <ghl234@nyu.edu>
2019-09-18 21:18:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -81,7 +82,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+  
+  programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages: ["LISP", "C"]
+  }
     
 end
 
